ff2cb4f32ca864c4d6f59681f740f1455c6cc571
rainitish034-bit <rainitish034@gmail.com>
2026-06-06 21:08:49 +0530

    upadate

---
+++ Day1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Day1{
+    public static void main(String[] args) {
+        //System.out.println("Hello World");
+        //int num1 = 4;
+        //int num2 = 5;
+        //int num3 = 7;
+        //int sum = num1 + num2 + num3;
+        //System.out.println(sum);
+        //byte age = 43;
+        //char ch = 'A';
+      //  float f1 = 5.6f;
+      //  double d1 = 8.44D;
+      //  System.err.println(age);
+      //  String str = "nitish";
+      //  System.out.println(str);
+        System.out.println("Taking Input Form the user");
+        Scanner sc = new Scanner(System.in);
+      //  System.out.println("Enter Number 1");
+      //  int a = sc.nextInt();
+       // System.out.println("Enter Number 2");
+        //int b = sc.nextInt();
+        //int Sum = a +b;
+        //System.out.println("The sum of these Number is");
+      //  System.out.println(Sum);
+      // just experiment
+      boolean b1 = sc.hasNextInt();
+      System.out.println(b1);
+    }
+}
