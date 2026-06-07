c17aa2a04322c92ee899a8185cb262049f9a6228
rainitish034-bit <rainitish034@gmail.com>
2026-06-07 10:24:51 +0530

    new update

--- Exercise1.java
+++ Exercise1.java
@@ -2,21 +2,41 @@ import java.util.Scanner;
 
 public class Exercise1 {
     public static void main(String[] args) {
+        //question1
 Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number 1");
-       int a = sc.nextInt();
-       System.out.println("Enter Number 2");
-       int b = sc.nextInt();
-       System.out.println("Enter Number 3");
-       int c = sc.nextInt();
-       System.out.println("Enter Number 4");
-       int d = sc.nextInt();
-       System.out.println("Enter Number 5");
-       int e = sc.nextInt();
-       int sum = a+b+c+d+e;
-        System.out.println("sum =" + sum);
-        double percentage = (sum * 100)/500;
-         System.out.println("percentage =" + percentage + "%");
-
+       // System.out.println("Enter Number 1");
+       //int a = sc.nextInt();
+       //System.out.println("Enter Number 2");
+       //int b = sc.nextInt();
+       //System.out.println("Enter Number 3");
+       //int c = sc.nextInt();
+       //System.out.println("Enter Number 4");
+       //int d = sc.nextInt();
+       //System.out.println("Enter Number 5");
+       //int e = sc.nextInt();
+       //int sum = a+b+c+d+e;
+        //System.out.println("sum =" + sum);
+        //double percentage = (sum * 100)/500;
+        // System.out.println("percentage =" + percentage + "%");
+         // question 2
+        // int num1 = 4;
+        //int num2 = 5;
+        //int num3 = 7;
+        //int sum = num1 + num2 + num3;
+        //System.out.println(sum);
+        //question 3
+        //float subject1 = 40;
+        //float subject2= 55;
+        //float subject3 = 67;
+        //float cgpa =( subject1 + subject2 + subject3)/30;
+        //System.out.println(cgpa);
+        // question 4
+       // System.out.println("what is your name" );
+        //Scanner sc = new Scanner(system.in); but this line is uper side of code
+        // String name = sc.next();
+        // System.out.println("hello "  + name +  " have a good day!");
+        // question 5
+        System.out.println(" Enter your number");
+        System.out.println(sc.hasNextInt());
     }
 }
