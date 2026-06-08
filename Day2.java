4176a618ab36ceb7410d630ff37297da4c9ea2bf
rainitish034-bit <rainitish034@gmail.com>
2026-06-08 12:26:48 +0530

    code right

--- Day2.java
+++ Day2.java
@@ -40,7 +40,7 @@ public class Day2{
         int b = 1;
         int c = 4;
         int a= 5;
-         int k = b*b -( 4*a*c)/(2*a);
+         int k = (b*b) - 4*a*c/(2*a);
         System.out.println(k);
     */
      /* byte x = 5;
---
+++ Exercise2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+// Q1. what will be the result of the following expression float a= 7/4*9/2
+public class Exercise2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //float a = 7/4.0f  * 9/2.0f;
+        //System.out.println(a);
+
+        //Q2 write a java program to encrypt a a grade by adding 8 to it decrypt it to show the correct grade.
+        //char grade ='b';
+        //grade =(char)  (grade +8);
+        //System.out.println(grade);
+        //decrypt
+        //grade =(char)  (grade -8);
+        //System.out.println(grade);
+        
+        
+    }
+}
