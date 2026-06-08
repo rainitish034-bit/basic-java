1c84730ff197b0256194b986be5eb5df8180cc63
rainitish034-bit <rainitish034@gmail.com>
2026-06-08 17:53:17 +0530

    conditon,operator and so on

--- Day3.java
+++ Day3.java
@@ -4,10 +4,10 @@ public class Day3{
         //System.out.println(name);
        // int value = name.length();
         //System.out.println(value);
-        String lString= name.toLowerCase();
-        System.out.println(lString);
-       String uString= name.toUpperCase();
-        System.out.println(uString);
+        //String lString= name.toLowerCase();
+        //System.out.println(lString);
+       //String uString= name.toUpperCase();
+        //System.out.println(uString);
         //String nonTrimmedString = "   Nitish   ";
         //System.out.println(nonTrimmedString);
        // System.out.println(name.substring(2));
@@ -16,7 +16,40 @@ public class Day3{
        //System.out.println(name.endsWith("sh")); 
        //System.out.println(name.charAt(4));
        //System.out.println(name.indexOf("s"));
-       System.out.println(name.indexOf("t",3));
-        
+       //System.out.println(name.indexOf("t",3));
+       //CONDITION
+       //int age =6;
+       //if(age>18){
+       // System.out.println("yes");
+      // }
+      // else{
+       // System.out.println("no");
+      // }
+      // LOGICAL OPERATOR
+      //System.out.println("Logical AND");
+     // boolean a = true;
+     // boolean b = false;
+     // if (a && b){
+     // System.out.println("yes");
+     // }
+      //else {
+        // System.out.println("no");
+     // }
+     //System.out.println("Logical OR");
+       // boolean a = true;
+      //boolean b = false;
+      //if (a || b){
+      //System.out.println("yes");
+      //}
+      //else {
+       //  System.out.println("no");
+     // }
+      System.out.println("Logical NOT");
+        boolean a = true;
+      boolean b = false;
+      System.out.println("Not (a) is");
+      System.out.println(!a);
+      System.out.println("Not (b) is");
+      System.out.println(!b);
     }
 }
