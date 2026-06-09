60d2fa11379932711704ac98c23ea224f1dd225f
rainitish034-bit <rainitish034@gmail.com>
2026-06-09 09:59:02 +0530

    new

---
+++ Day4.java
@@ -0,0 +1,49 @@
+
+
+public class Day4{
+    public static void main(String[] args) {
+       /* int age;
+        System.out.println("Enter your age");
+        Scanner sc = new Scanner(System.in);
+        age = sc.nextInt();
+          if (age>56){
+        System.out.println("you are exprienced");
+           }
+           else if(age>46){
+            System.out.println("you are semi experienced");
+          }
+         else if(age>39){
+            System.out.println("yes you are more semi experienced");
+        }
+        else{
+            System.out.println("you are not exprienced");
+        } 
+       switch (age) {
+           case 18:
+               System.out.println("you are goining adult");
+               break;
+               case 21:
+               System.out.println("you are not goining to  join a job");
+                break;
+                case 23:
+               System.out.println("you are not goining to  get retired a job");
+                break;
+           default:
+               System.out.println("Enjoy your life");
+       }*/
+            char  var = 'r';
+        switch (var) {
+           case 'r':
+               System.out.println("you are goining adult");
+               break;
+               case 'b':
+               System.out.println("you are not goining to  join a job");
+                break;
+                case 'c':
+               System.out.println("you are not goining to  get retired a job");
+                break;
+           default:
+               System.out.println("Enjoy your life");
+       }
+   }
+}
