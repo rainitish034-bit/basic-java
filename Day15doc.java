851f59ef9b09b2b19bb80b9efb60ef5dab4d935d
rainitish034-bit <rainitish034@gmail.com>
2026-06-22 17:27:43 +0530

    file name change

---
+++ Day15doc.java
@@ -0,0 +1,8 @@
+public class Day15doc{
+        public void add( int a, int b){
+      System.out.println("The sum is: " + a+b);
+        } 
+   public static void main(String[] args) {
+    System.out.println("this is my main method");
+    }
+}
