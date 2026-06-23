3f7de79742c7aa616c464883e5d66d085074e2e3
rainitish034-bit <rainitish034@gmail.com>
2026-06-23 12:30:18 +0530

    code change

--- Day15doc.java
+++ Day15doc.java
@@ -2,7 +2,23 @@ public class Day15doc{
         public void add( int a, int b){
       System.out.println("The sum is: " + a+b);
         } 
+        /**
+         * 
+         * @param arg this are arguments supplied to the command line
+         */
    public static void main(String[] args) {
     System.out.println("this is my main method");
     }
-}
+    /**
+     * 
+     * @param i this is the first number to add
+     * @param j this is the second number to add
+     * @returen sum of two number in intger
+     */
+    public int sum(int i, int j){
+      int c;
+      c = i+j;
+      return c;
+    }
+}    
+   
